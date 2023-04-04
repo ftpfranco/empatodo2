c36574925bf41dbe71d3586a001fb68b1105fc55
F1703 <franco170391@gmail.com>
2023-04-04 17:01:57 -0300

    asdf

---
+++ .fly/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# Run user scripts, if they exist
+for f in /var/www/html/.fly/scripts/*.sh; do
+    # Bail out this loop if any script exits with non-zero status code
+    bash "$f" || break
+done
+chown -R webuser:webgroup /var/www/html
+
+if [ $# -gt 0 ]; then
+    # If we passed a command, run it as root
+    exec "$@"
+
+    #nohup /usr/bin/php /var/www/html/artisan queue:work  --sleep=3 --tries=3 --timeout=3600   </dev/null &>/dev/null &
+
+else
+    exec /init
+fi
