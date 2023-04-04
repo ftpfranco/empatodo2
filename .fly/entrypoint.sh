5182b1b343bbe2d61527cbdb339b22756eeb5805
F1703 <franco170391@gmail.com>
2023-04-04 20:15:37 -0300

    adfasdf

--- .fly/entrypoint.sh
+++ .fly/entrypoint.sh
@@ -16,7 +16,7 @@ if [ $# -gt 0 ]; then
     #  nohup /usr/bin/php /var/www/html/artisan queue:work  --sleep=3 --tries=3 --timeout=3600   </dev/null &>/dev/null &
 
 else
-    nohup /usr/bin/php /var/www/html/artisan queue:work  --sleep=3 --tries=3 --timeout=3600   </dev/null &>/dev/null &
     exec /init
 fi
 
+nohup /usr/bin/php /var/www/html/artisan queue:work  --sleep=3 --tries=3 --timeout=3600   </dev/null &>/dev/null &
