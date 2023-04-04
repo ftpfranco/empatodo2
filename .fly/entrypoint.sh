2756c49179924b9cd2dd9c3bb3a6ca1fd1622ac9
F1703 <franco170391@gmail.com>
2023-04-04 19:05:35 -0300

    asf

--- .fly/entrypoint.sh
+++ .fly/entrypoint.sh
@@ -6,14 +6,16 @@ for f in /var/www/html/.fly/scripts/*.sh; do
     bash "$f" || break
 done
 # chown -R webuser:webgroup /var/www/html
-chown -R www-data:www-data /var/www/html
+# chown -R www-data:www-data /var/www/html
 
 if [ $# -gt 0 ]; then
     # If we passed a command, run it as root
     exec "$@"
 
+    # exec nohup /usr/bin/php /var/www/html/artisan queue:work  --sleep=3 --tries=3 --timeout=3600   </dev/null &>/dev/null &
 
-# else
-#     exec /init
+else
+    # exec /init
+    exec nohup /usr/bin/php /var/www/html/artisan queue:work  --sleep=3 --tries=3 --timeout=3600   </dev/null &>/dev/null &
 fi
-exec nohup /usr/bin/php /var/www/html/artisan queue:work  --sleep=3 --tries=3 --timeout=3600   </dev/null &>/dev/null &
+
