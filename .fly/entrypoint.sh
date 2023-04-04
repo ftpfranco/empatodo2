a0b09e5f8da717cc25f9b8bc5d1f0f77a7d1036a
F1703 <franco170391@gmail.com>
2023-04-04 20:29:44 -0300

    asdfs

--- .fly/entrypoint.sh
+++ .fly/entrypoint.sh
@@ -16,7 +16,7 @@ if [ $# -gt 0 ]; then
     #  nohup /usr/bin/php /var/www/html/artisan queue:work  --sleep=3 --tries=3 --timeout=3600   </dev/null &>/dev/null &
 
 else
-    exec supervisord -c /etc/supervisor/supervisord.conf
+    exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
     exec /init
 fi
 
