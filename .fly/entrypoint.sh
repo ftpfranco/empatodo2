0035199e2ceb3c74cc78aa0890f6f0b743b3a70a
F1703 <franco170391@gmail.com>
2023-04-04 21:12:45 -0300

    asfsd

--- .fly/entrypoint.sh
+++ .fly/entrypoint.sh
@@ -17,6 +17,7 @@ if [ $# -gt 0 ]; then
     exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 
 else
+    exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
     exec /init
 fi
 
